993ab3da84b66c6334118634c4fd42e77dcf3326
aap <aap@papnet.eu>
2020-05-08 09:34:21 +0200

    pushing old changes

--- build.cmd
+++ build.cmd
@@ -4,6 +4,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32
 
 cl /LD /I %RWSDK34% /I C:\Users\aap\src\lpng1634 "C:\Users\aap\src\lpng1634\projects\vstudio\Release Library\libpng16.lib" "C:\Users\aap\src\lpng1634\projects\vstudio\Release Library\zlib.lib" screenshot.cpp /Fescreenshot.dll
 cl /LD betacam.cpp /Febetacam.dll
+cl /LD betahud.cpp /Febetahud.dll
 cl /LD vehicleflare.cpp /Fevehicleflare.dll
 cl /LD iiifont.cpp /Feiiifont.dll
 cl /LD noreplay.cpp /Fenoreplay.dll
@@ -16,3 +17,5 @@ cl /LD overridecd.cpp /Feoverridecd.dll
 cl /LD portablegta.cpp /Feportablegta.dll
 cl /LD hydraulics.cpp /Fehydraulics.dll
 cl /LD regionfree.cpp /Feregionfree.dll
+cl /LD noskinshader.cpp /Fenoskinshader.dll
+cl /LD uncensor.cpp /Feuncensor.dll
--- noskin.cpp
+++ noskin.cpp
@@ -26,13 +26,13 @@ struct MenuEntry
 };
 struct MenuScreen
 {
-  char name[8];
-  int32 unk1;
-  int32 parent1;
-  int32 parent2;
-  int32 unk2;
-  int32 unk3;
-  MenuEntry entries[18];
+	char name[8];
+	int32 unk1;
+	int32 parent1;
+	int32 parent2;
+	int32 unk2;
+	int32 unk3;
+	MenuEntry entries[18];
 };
 MenuScreen *optionsScreen = (MenuScreen*)0x615754;
 
@@ -44,11 +44,39 @@ struct GlobalScene
 GlobalScene &Scene = *(GlobalScene*)0x726768;
 
 void
+patchVC10(void)
+{
+	int n;
+	// remove player skin setup
+	for(n = 0; optionsScreen->entries[n].name[0]; n++)
+		if(strcmp(optionsScreen->entries[n].name, "FET_PSU") == 0)
+			goto found;
+	return;
+found:
+	for(; n < 17; n++)
+		optionsScreen->entries[n] = optionsScreen->entries[n+1];
+	memset(&optionsScreen->entries[17], 0, sizeof(MenuEntry));
+
+	// don't init skin
+//	Nop(0x48C09A, 5);
+//	struct{uchar m[3];} m = { {0xc2, 0x04, 0x00} };
+//	Patch(0x4A16D0, m);
+}
+
+void
 patchIII10(void)
 {
+	int n;
 	// remove player skin setup
-	optionsScreen->entries[4] = optionsScreen->entries[5];
-	memset(&optionsScreen->entries[5], 0, sizeof(MenuEntry));
+	for(n = 0; optionsScreen->entries[n].name[0]; n++)
+		if(strcmp(optionsScreen->entries[n].name, "FET_PSU") == 0)
+			goto found;
+	return;
+found:
+	for(; n < 17; n++)
+		optionsScreen->entries[n] = optionsScreen->entries[n+1];
+	memset(&optionsScreen->entries[17], 0, sizeof(MenuEntry));
+
 	// don't init skin
 	Nop(0x48C09A, 5);
 	struct{uchar m[3];} m = { {0xc2, 0x04, 0x00} };
@@ -60,8 +88,10 @@ DllMain(HINSTANCE hInst, DWORD reason, LPVOID)
 {
 	if(reason == DLL_PROCESS_ATTACH){
 		AddressByVersion<uint32_t>(0, 0, 0, 0, 0, 0);
-		if (gtaversion == III_10)
+		if(gtaversion == III_10)
 			patchIII10();
+		else if(gtaversion == VC_10)
+			patchVC10();
 	}
 
 	return TRUE;
